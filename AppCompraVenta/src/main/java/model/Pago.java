68ef1cf43356d67c24fad0dc58841efd6f150996
Krish Sadhwani <92883393+KrishT97@users.noreply.github.com>
2022-12-26 15:48:04 +0000

    Update Pago.java

--- AppCompraVenta/src/main/java/model/Pago.java
+++ AppCompraVenta/src/main/java/model/Pago.java
@@ -4,7 +4,7 @@ public class Pago {
     public static String realizarPago(String pago) {
 
         return "Se va realizar el pago por nombre de: "+Usuario.getNombre()+" con metodo de pago: " +pago+",siendo en total:" + Carro.getPrecioTotal() +
-                ",en la ubicacion de " + Cliente.getShipAddress() + ". Redirigiendo a la web para pagar por " + pago;
+                ",en la ubicacion de " + Cliente.getShipAddress() + ". Redirigiendo a la web para pagar por " + pago + ".";
 
     }
 }
